0610219aa8d356988f31d409167943f302842910
Oren Zomer <oren.zomer@gmail.com>
2021-09-10 18:37:19 +0300

    Docker build for getumbrel

--- docker-custom/docker-debian.sh
+++ docker-custom/docker-debian.sh
@@ -5,12 +5,14 @@ echo "#########################################################################"
 echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
-docker build --rm --no-cache \
-    --build-arg ARCH=amd64 \
+docker buildx build --rm --no-cache \
+    --platform linux/arm64,linux/amd64 \
+    --build-arg ARCH=arm64_amd64 \
     --build-arg NODE_VERSION=12 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
     --build-arg OS=buster-slim \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.debian \
-    --tag testing:node-red-build .
+    --tag orenzomer/node-red-build:v${NODE_RED_VERSION} \
+    --output "type=registry" .
